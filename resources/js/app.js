5a2a7f1a2ccc33919f0ff3416d743521fe50d1d2
Simarpreet Singh <simarpreet.singh@canvascraft.media>
2022-06-20 09:01:47 +0530

    Toastr and add-to-cart-button added

--- resources/js/app.js
+++ resources/js/app.js
@@ -13,6 +13,7 @@ import Vue from 'vue';
 import VueToastr from 'vue-toastr';
 Vue.use(VueToastr);
 
+
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
